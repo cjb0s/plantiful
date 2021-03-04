20b437a6c14cf9baddac74ca7b770a85bbe73b53
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-04 13:10:38 +0000

    refactor(controllers): removed console.log

--- server/controllers/plant.js
+++ server/controllers/plant.js
@@ -5,7 +5,6 @@ const Plant = require('../models/plant');
 exports.getPlants = async (req, res) => {
   try {
     const plants = await Plant.find();
-    console.log('hello');
     res.status(200);
     res.send(plants);
   } catch (error) {
