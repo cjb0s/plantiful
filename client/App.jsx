07b0b5389988922d8fda37c5117a4e289b256ce4
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-04 11:05:47 +0000

    build(navigation): remove stack navigation package

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ApiService from './services/ApiService';
-import { createStackNavigator } from '@react-navigation/stack';
 import MyPlants from './screens/MyPlants/MyPlants';
-
-const Stack = createStackNavigator();
 
 export default function App() {
   const [plants, setPlants] = useState([]);
